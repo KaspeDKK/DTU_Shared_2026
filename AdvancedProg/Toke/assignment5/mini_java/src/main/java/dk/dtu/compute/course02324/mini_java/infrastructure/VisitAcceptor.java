56acc2ecdbb8984960a49b3d1c43a74473247f91
Tokemeister <144561301+Tokemeister@users.noreply.github.com>
2026-03-05 22:28:52 +0100

    div and mod

--- AdvancedProg/Toke/assignment5/mini_java/src/main/java/dk/dtu/compute/course02324/mini_java/infrastructure/VisitAcceptor.java
+++ AdvancedProg/Toke/assignment5/mini_java/src/main/java/dk/dtu/compute/course02324/mini_java/infrastructure/VisitAcceptor.java
@@ -1,4 +1,4 @@
-package dk.dtu.compute.course02324.mini_java.infrastructure;
+wpackage dk.dtu.compute.course02324.mini_java.infrastructure;
 
 import dk.dtu.compute.course02324.mini_java.semantics.ProgramVisitor;
 
