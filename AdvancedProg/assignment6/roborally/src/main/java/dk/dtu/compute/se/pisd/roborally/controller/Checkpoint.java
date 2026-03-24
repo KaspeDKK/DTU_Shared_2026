64c5d324d4d2af6aa93e24574a910322ca1aa482
Tokemeister <toketorrild@icloud.com>
2026-03-24 11:50:43 +0100

    bleepbloop

---
+++ AdvancedProg/assignment6/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
@@ -0,0 +1,15 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+import org.jetbrains.annotations.NotNull;
+
+public class Checkpoint extends FieldAction {
+
+    public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
+        // TODO A6d: needs to be implemented
+        // ...
+
+        return false;
+    }
+}
+
