93244ff054292fba21627c6f1a88a2241fc0ca28
KaspeDKK <79929855+KaspeDKK@users.noreply.github.com>
2026-05-03 21:40:43 +0200

    bertter readabkyrgdfg

--- MOP_2/Controller/controller.c
+++ MOP_2/Controller/controller.c
@@ -383,7 +383,7 @@ void moveCard(Card *moveCard, Column *columnFrom, Column *columnTo) {
         headCard->next = NULL;
         endOfColumn->next = cardToMove;
     } else {
-        printf("Illegal move");
+        printf("Illegal move\n");
     }
 }
 
