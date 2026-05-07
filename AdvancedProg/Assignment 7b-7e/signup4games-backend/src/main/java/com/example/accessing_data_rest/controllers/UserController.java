8acf23c239a2abb5226301ec21aa814749a5edc1
Friisma <magnusfriis0101@gmail.com>
2026-05-07 18:51:53 +0200

    added comments

--- AdvancedProg/Assignment 7b-7e/signup4games-backend/src/main/java/com/example/accessing_data_rest/controllers/UserController.java
+++ AdvancedProg/Assignment 7b-7e/signup4games-backend/src/main/java/com/example/accessing_data_rest/controllers/UserController.java
@@ -20,12 +20,25 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * endpoint for searching users by name.
+     * The method calls the searchUsers method of the UserService to perform the search and returns the result as a response to the client.
+     * @param name
+     * @return name as JSON
+     */
     @GetMapping(value = "/search", produces = "application/json")
     public List<User> searchUsers(@RequestParam("name") String name) {
         return userService.searchUsers(name);
     }
 
-    //TODO javadoc
+    /**
+     * endpoint for signing up a new user.
+     * The method takes a User object as input, which is expected to be in JSON format in the request body.
+     * The method calls the createUser method of the UserService to create a new user in the repository
+     * and returns the created user as a response to the client in JSON format.
+     * @param user
+     * @return user in JSON Format
+     */
     @PostMapping(
             value = "",
             consumes = "application/json",
@@ -34,7 +47,7 @@ public class UserController {
     public User SignUp(@RequestBody User user) {
         return userService.createUser(user);
     }
-    // TODO javadoc til exceptionhandler
+
     //Catches the CouldNotCreateUserException and returns a response with the exception message
     // and a 409 Conflict status code instead of 500 to the client
 
